d2064b25f457febeadc4a7fb7e3b3e9199457b39
ujoaovictor <jv664587@gmail.com>
2026-04-06 12:56:31 -0300

    Add Main class with example restaurant bill

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        
+        Cliente cliente = new Cliente("Joao Victor", 01);
+        
+        Pedido p1 = new Pedido("Pizza Grande", 40.00, 1);
+        Pedido p2 = new Pedido("Refrigerante 2L ", 10.00, 2);
+        Pedido p3 = new Pedido("Sobremesa", 18.00, 1);
+
+        
+        ContaRestaurante conta = new ContaRestaurante(cliente, p1, p2, p3);
+
+        
+        conta.mostrarConta();
+    }
